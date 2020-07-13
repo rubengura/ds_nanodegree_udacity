f01f9102af4d73bfc229ffeeaaa754e779b475d1
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-07-13 22:39:01 +0200

    Added preprocessors classes.

---
+++ finding_donors/preprocessors.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+
+from __future__ import annotations
+from sklearn.base import BaseEstimator, TransformerMixin
+import typing as t
+
+
+# frequent label categorical encoder
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, tol=0.05, variables=None):
+
+        self.tol = tol
+
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
+
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            # frequent labels:
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(X[feature].isin(self.encoder_dict_[
+                                                      feature]), X[feature], 'Rare')
+
+        return X
+
+    # string to numbers categorical encoder
+    class CategoricalEncoder(BaseEstimator, TransformerMixin):
+
+        def __init__(self, variables=None):
+            if not isinstance(variables, list):
+                self.variables = [variables]
+            else:
+                self.variables = variables
+
+        def fit(self, X: pd.DataFrame, y: pd.DataFrame) -> CategoricalEncoder:
+            temp = pd.concat([X, y], axis=1)
+            temp.columns = list(X.columns) + ['target']
+
+            # persist transforming dictionary
+            self.encoder_dict_ = {}
+
+            for var in self.variables:
+                t = temp.groupby([var])['target'].mean().sort_values(ascending=True).index
+                self.encoder_dict_[var] = {k: i for i, k in enumerate(t, 0)}
+
+            return self
+
+        def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+            # encode labels
+            X = X.copy()
+            for feature in self.variables:
+                X[feature] = X[feature].map(self.encoder_dict_[feature])
+
+            return X
+
+    # logarithm transformer
+    class LogTransformer(BaseEstimator, TransformerMixin):
+
+        def __init__(self, variables=None):
+            if not isinstance(variables, list):
+                self.variables = [variables]
+            else:
+                self.variables = variables
+
+        def fit(self, X: pd.DataFrame, y=None) -> LogTransformer:
+            # to accomodate the pipeline
+            return self
+
+        def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+            X = X.copy()
+
+            for feature in self.variables:
+                X[feature] = np.log(X[feature])
+
+            return X
