124e9c999d41c33db381d9cf5fb9b959ddba6519
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-07-13 22:16:15 +0200

    Added config variables for pipeline.

---
+++ finding_donors/config.py
@@ -0,0 +1,18 @@
+__author__ = "rgr"
+
+# data
+TRAINING_DATA_FILE = 'census.csv'
+PIPELINE_NAME = 'finding_donors_pipeline'
+
+TARGET = 'income'
+
+# input variables
+FEATURES = ['age', 'workclass', 'education_level', 'education_num', 'marital_status', 'occupation', 'relationship',
+            'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country']
+
+# variables to log transform
+NUMERICAL_LOG_VARS = ['capital_gain', 'capital_loss']
+
+# categorical variables to encode
+CATEGORITCAL_VARS = ['workclass', 'education_level', 'marital_status', 'occupation', 'relationship', 'race', 'sex',
+                     'native_country']
